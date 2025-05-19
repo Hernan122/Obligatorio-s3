7633fcb216243a1fa25097b4e05ca41091660243
FACULTADES\alumnoFI <alumnoFI>
2025-05-19 10:11:31 -0300

    5 de 6

--- LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
+++ LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62c84bd6e4d236db06df91770246f74dfb2d40a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948e8a07ae1984a9712ff3a61c29f51d70d87180")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
