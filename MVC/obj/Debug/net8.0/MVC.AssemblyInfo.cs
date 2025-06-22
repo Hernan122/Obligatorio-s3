7bbfb1b79b4c65b6e45c327065189f9f64a22649
BrThompson2312 <brunogomez2312@gmail.com>
2025-06-22 18:23:41 -0300

    probando Azure

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646d11aa1dd7c7ecb5c1e8dba1e9a971bd545e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5835303607ff987099cbb72cf5e480f6a38c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
